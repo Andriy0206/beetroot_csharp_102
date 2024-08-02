e82dafd6a9bbf9cee33b1ba589aa87c527d46fa4
Andriy0206 <les.andriy75@gmail.com>
2024-08-02 17:14:30 +0300

    Update Program.cs

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,11 +1,22 @@
 ﻿//Homework
-double x = 2;
-double y = 3;
-double result1 = -6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15;
-double result2 = Math.Abs(x) * Math.Sin(x);
-double result3 = 2 * Math.PI * x;
-double result4 = Math.Max(x,y);
+using System.Threading.Tasks;
+
+var x = 2;
+var y = 3;
+var result1 = -6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15;
+var result2 = Math.Abs(x) * Math.Sin(x);
+var result3 = 2 * Math.PI * x;
+var result4 = Math.Max(x,y);
 Console.WriteLine(result1);
 Console.WriteLine(result2);
 Console.WriteLine(result3);
 Console.WriteLine(result4);
+
+//New Year
+var dateTimeToday = DateTime.Today;
+var dateTimeNewYear =  DateTime(dateTimeToday.Year, 1, 1);
+var dateTimeNewYearNext =  DateTime(dateTimeToday.Year + 1, 1, 1);
+var daysNextYear = (dateTimeNewYearNext - dateTimeToday).TotalDays;
+var daysThisYear = dateTimeToday.DayOfYear;
+Console.WriteLine("left to the New Year");
+Console.WriteLine("days passed from New Year");
